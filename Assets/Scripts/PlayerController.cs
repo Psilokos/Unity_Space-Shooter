e614d6bc59a5d7a5d29e8f2b58948d04f816c9fc
Victorien LE COUVIOUR--TUFFET <victorien.lecouviour-tuffet@epitech.eu>
2015-02-06 15:35:09 +0100

    Movements / Wall detection

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float    speed;
+    public float    tilt;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float   h_move      = Input.GetAxis("Horizontal");
+        float   v_move      = Input.GetAxis("Vertical");
+        Vector3 movement    = new Vector3(h_move, 0.0f, v_move);
+
+        this.rigidbody.velocity = movement * this.speed;
+        this.rigidbody.position = new Vector3
+        (
+            Mathf.Clamp(this.rigidbody.position.x, this.boundary.xMin, this.boundary.xMax),
+            0.0f,
+            Mathf.Clamp(this.rigidbody.position.z, this.boundary.zMin, this.boundary.zMax)
+        );
+        this.rigidbody.rotation = Quaternion.Euler(this.rigidbody.velocity.z * this.tilt / 1.5f, 0.0f, this.rigidbody.velocity.x * -this.tilt);
+    }
+}
